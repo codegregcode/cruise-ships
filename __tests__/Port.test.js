e43d91f90eca3e4789ced34d74f55afd133b83fc
Greg Cain <codegregcode@gmail.com>
2022-12-15 16:13:13 +0000

    DRYs up ship and port tests

--- __tests__/Port.test.js
+++ __tests__/Port.test.js
@@ -3,30 +3,32 @@ const Port = require('../src/Port');
 const Ship = require('../src/Ship');
 const Itinerary = require('../src/Itinerary');
 describe('Port', () => {
+    describe('monitors ships in and leaving port', () => {
+        let plymouth;
+        let ship;
+        let titanic;
+        let queenMary;
+        beforeEach(() => {
+            plymouth = new Port('Plymouth');
+            ship = {};
+            titanic = {};
+            queenMary = {};
+    });
     it('can be instantiated', () => { 
         expect(new Port()).toBeInstanceOf(Object);
     });
     it('checks port object to have name', () => {
-        const port = new Port('Plymouth');
-        expect(port.name).toBeTruthy();
+        expect(plymouth.name).toBeTruthy();
     });
-});
-describe('addShip', () => {
     it('can add a ship', () => {
-        const port = new Port('Plymouth');
-        const ship = {};
-        port.addShip(ship);
-        expect(port.ships).toContain(ship);
+        plymouth.addShip(ship);
+        expect(plymouth.ships).toContain(ship);
     });
-});
-describe('removeShip', () => {
     it('can remove a ship', () => {
-        const port = new Port('Plymouth');
-        const titanic = {};
-        const queenMary = {};
-        port.addShip(titanic);
-        port.addShip(queenMary);
-        port.removeShip(queenMary);
-        expect(port.ships).toEqual([titanic]);
+        plymouth.addShip(titanic);
+        plymouth.addShip(queenMary);
+        plymouth.removeShip(queenMary);
+        expect(plymouth.ships).toEqual([titanic]);
+    });
     });
 });
--- __tests__/Ship.test.js
+++ __tests__/Ship.test.js
@@ -3,54 +3,46 @@ const Ship = require('../src/Ship.js');
 const Port = require('../src/Port.js');
 const Itinerary = require('../src/Itinerary');
 describe('Ship', () => {
-    it('can be instantiated', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-        expect(ship).toBeInstanceOf(Object);
+    describe('with ports, a sail method, dock method and an itinerary', () => {
+        let plymouth;
+        let topsham;
+        let itinerary;
+        let ship;
+        beforeEach(() => {
+            plymouth = new Port('Plymouth');
+            topsham = new Port('Topsham');
+            itinerary = new Itinerary([plymouth, topsham]);
+            ship = new Ship(itinerary);
     });
-    it('has a starting port', () => {
-        const port = new Port('Dover');
-        const itinerary = new Itinerary([port]);
-        const ship = new Ship(itinerary);
-        expect(ship.currentPort).toBe(port);
-    });
-    it('gets added to port on instantiation', () => {
-        const plymouth = new Port('Plymouth');
-        const itinerary = new Itinerary([plymouth]);
-        const ship = new Ship(itinerary);
-        expect(plymouth.ships).toContain(ship);
-    });
-});
-describe('setSail', () => {
-    it('can set sail', () => {
-        const port = new Port('Dover');
-        const aPort = new Port('Calais');
-        const itinerary = new Itinerary([port, aPort]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        expect(ship.currentPort).toBeFalsy();
-        expect(port.ships).not.toContain(ship);
-    });
-    it('can\'t sail further than its itinerary', () => {
-        const plymouth = new Port('Plymouth');
-        const topsham = new Port('Topsham');
-        const itinerary = new Itinerary([plymouth, topsham]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(() => ship.setSail()).toThrowError('End of itinerary reached');
-    });
-});
-describe('dock', () => {
-    it('can dock at another port', () => {
-        const plymouth = new Port('Plymouth');
-        const topsham = new Port('Topsham');
-        const itinerary = new Itinerary([plymouth, topsham]);
-        const ship = new Ship(itinerary);
-        ship.setSail();
-        ship.dock();
-        expect(ship.currentPort).toBe(topsham);
-        expect(topsham.ships).toContain(ship);
+        it('can be instantiated', () => {
+            expect(ship).toBeInstanceOf(Object);
+        });
+        it('has a starting port', () => {
+            expect(ship.currentPort).toBe(plymouth);
+        });
+        it('can set sail', () => {
+            ship.setSail();
+            expect(ship.currentPort).toBeFalsy();
+            expect(plymouth.ships).not.toContain(ship);
+        });
+        it('gets added to port on instantiation', () => {
+            expect(plymouth.ships).toContain(ship);
+        });
+        it('can set sail', () => {
+            ship.setSail();
+            expect(ship.currentPort).toBeFalsy();
+            expect(plymouth.ships).not.toContain(ship);
+        });
+        it('can\'t sail further than its itinerary', () => {
+            ship.setSail();
+            ship.dock();
+            expect(() => ship.setSail()).toThrowError('End of itinerary reached');
+        });
+        it('can dock at another port', () => {
+            ship.setSail();
+            ship.dock();
+            expect(ship.currentPort).toBe(topsham);
+            expect(topsham.ships).toContain(ship);
+        });
     });
 });
