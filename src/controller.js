4ae2ee9de38223825074ab2a086e89c4aa5a16fc
Greg Cain <codegregcode@gmail.com>
2023-01-06 12:03:31 +0000

    adds contollerjs to control objects in dom

---
+++ src/controller.js
@@ -0,0 +1,46 @@
+(function exportController() {
+    class Controller {
+        constructor() {
+            this.initialiseSea();
+        }
+
+        initialiseSea() {
+            const backgrounds = [
+                './image/water0.png',
+                './image/water1.png'
+            ];
+            let backgroundIndex = 0;
+            window.setInterval(() => {
+                document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+                backgroundIndex += 1;
+            }, 1000);
+        }
+
+        renderPorts(ports) {
+            const portsElement = document.querySelector('#ports');
+            portsElement.style.width = '0px';
+            ports.forEach((port, index) => {
+                const newPortElement = document.createElement('div');
+                newPortElement.className = 'port';  
+                newPortElement.dataset.portName = port.name;
+                newPortElement.dataset.portIndex = index;
+                portsElement.appendChild(newPortElement);
+                const portsElementWidth = parseInt(portsElement.style.width, 10);
+                portsElement.style.width = `${portsElementWidth + 256}px`;
+            })
+        }
+        renderShip(ship) {
+            const shipPortIndex = ship.itinerary.ports.indexOf(ship.currentPort);
+            const portElement = document.querySelector(`[data-port-index='${shipPortIndex}']`);
+            const shipElement = document.querySelector('#ship');
+            shipElement.style.top = `${portElement.offsetTop + 32}px`;
+            shipElement.style.left = `${portElement.offsetLeft - 32}px`;
+        }
+    }
+
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Controller;
+        } else {
+        window.Controller = Controller;
+    }
+}());
