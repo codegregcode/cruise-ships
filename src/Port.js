4d0454ef6a7b41a92857eec6b97cd9698867a1b6
Greg Cain <codegregcode@gmail.com>
2023-01-06 12:02:31 +0000

    makes objects browswer friendly

--- src/Port.js
+++ src/Port.js
@@ -1,14 +1,22 @@
-class Port {
-    constructor (name) {
-        this.name = name;
-        this.ships = [];
-    }
-    addShip(ship) {
-        this.ships.push(ship);
-    }
-    removeShip(ship) {
-        this.ships.splice(this.ships.indexOf(ship), 1);
-    }
-}
+(function exportPort() {
+    class Port {
+        constructor (name) {
+            this.name = name;
+            this.ships = [];
+        }
 
-module.exports = Port;
+        addShip(ship) {
+            this.ships.push(ship);
+        }
+
+        removeShip(ship) {
+            this.ships.splice(this.ships.indexOf(ship), 1);
+        }
+    };
+    
+    if (typeof module !== 'undefined' && module.exports) {
+        module.exports = Port;
+        } else {
+        window.Port = Port;
+    }
+}());
